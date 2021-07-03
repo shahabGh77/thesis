0916134b08fdb5b5618e109ae74813e5a4c8e36a
shahabGh77 <shahab16774@gmail.com>
2021-07-03 22:54:05 +0430

    remove unuseful comments

--- algorithms/HITS.py
+++ algorithms/HITS.py
@@ -1,11 +1,5 @@
 from pyspark.sql.functions import col, lit, when
 
-# from pyspark import SparkContext
-# sc = SparkContext("local", "First App")
-
-# from pyspark.sql import SQLContext
-# sqlContext = SQLContext(sc)
-
 
 def hubsNormalisation(V):
     max_h_score = V.agg({'h_score': 'max'}).collect()[0][0]
@@ -41,39 +35,3 @@ def HITS(V, E, iteration, hubs=None, auths=None, writeToFile=False):
 
     return hubs, auths
 
-
-
-
-# vertices = sqlContext.createDataFrame([
-#     ('A', ),
-#     ('B', ),
-#     ('C', ),
-#     ('D', ),
-#     ('E', )], ["id", ])
-
-
-# edges = sqlContext.createDataFrame([
-#     ('A', 'B'),
-#     ('A', 'C'),
-#     ('A', 'D'),
-#     ('B', 'A'),
-#     ('B', 'D'),
-#     ('C', 'E'),
-#     ('D', 'B'),
-#     ('D', 'C')], ["src", "dst", ])
-
-
-# hubs, auths = HITS(vertices, edges, 5)
-
-# hubs.show()
-# auths.show()
-
-# print('yesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss')
-# g = GraphFrame(vertices, edges)
-# print(g)
-
-# from igraph import Graph, plot
-# ig = Graph.TupleList(g.edges.collect(), directed=True)
-# visual_style = {}
-# visual_style["margin"] = 70
-# plot(ig)
